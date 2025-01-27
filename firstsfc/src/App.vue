f4adf10b27d7dc4393c3c81ae8178f322e695945
alcantara0422 <mmalcantara@student.apc.edu.ph>
2025-01-27 06:25:46 +0000

    Added components

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,7 +1,13 @@
-<script></script>
-
 <template>
-  <h1>Hello WEBPROG MI231 World!</h1>
+  <h1>Food</h1>
+  <personal-profile/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  
 </template>
 
-<style></style>
+<script></script>
+
+<style>
+</style>           
