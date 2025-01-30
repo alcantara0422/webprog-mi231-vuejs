10884c0d23b5d767392bcd9d850d6a29788f5602
alcantara0422 <mmalcantara@student.apc.edu.ph>
2025-01-30 06:52:47 +0000

    ADded supabase

---
+++ firstsfc/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://xomupcdparpckllixvwq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvbXVwY2RwYXJwY2tsbGl4dndxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTg3MjMsImV4cCI6MjA1Mzc5NDcyM30.9m6ZMw2XlYNoR-3WZOB_6h6rITU9ECereilVFqtdMig')
